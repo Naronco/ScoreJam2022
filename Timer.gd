4c9551052d35d21c750fbc16b07d54c9c5c64c1b
Johannes Roth <johannes.roth18199@gmail.com>
2022-12-19 19:36:42 +0100

    dadsa erom erom
    add timer

---
+++ Timer.gd
@@ -0,0 +1,25 @@
+extends Label
+
+
+var timeRemaining = 3*60.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	timeRemaining -= delta
+	if timeRemaining < 0.0:
+		# loose
+		timeRemaining = 0.0
+	
+	var sec = int(timeRemaining)
+	var min = floor(sec / 60)
+	sec %= 60
+	
+	text = ("0" + str(min) if min<10 else str(min)) + ":" + ("0" + str(sec) if sec<10 else str(sec)) + " min"
+	
+	pass
